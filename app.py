0d9b5583124078e7261506b61a1112cb5bc6fbe8
Dongha-code <ddoyo@nethru.co.kr>
2025-04-16 14:20:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def submit():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", port=10000)
+
